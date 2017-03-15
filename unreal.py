0bb6ab2b9e74892c30b94c6e8feb17872d19ae71
NoobFang <fhy881229@163.com>
2017-03-15 23:54:13 +0800

    'little work'

--- unreal.py
+++ unreal.py
@@ -11,10 +11,17 @@ class UNREAL(object):
     self.task = task
     worker_device = '/job:worker/task:{}'.format(task)
     with tf.device(tf.train.replica_device_setter(1, worker_device=worker_device)):
-      self.network = UnrealNetwork(env.observation_space.shape, 
-                                  env.action_space.n)
-      self.global_step = tf.get_variable('global_step', [], tf.int32, 
-                                         initializer=tf.constant(0, dtype=tf.int32),
-                                         trainable=False)
+      with tf.variable_scope('global'):
+        self.network = UnrealNetwork(env.observation_space.shape, 
+                                    env.action_space.n)
+        self.global_step = tf.get_variable('global_step', [], tf.int32, 
+                                          initializer=tf.constant(0, dtype=tf.int32),
+                                          trainable=False)
 
     with tf.device(worker_device):
+      with tf.variable_scope('local'):
+        self.local_network = pi = UnrealNetwork(env.observation_space.shape, 
+                                           env.action_space.n)
+        pi.global_step = self.global_step
+      
+      # TODO: define the input and loss of UNREAL model
