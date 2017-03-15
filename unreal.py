5b07d614cbf2454a047d06e7cbb5fa8dfe70ec17
fhy-zju <fhy@fhy-mbp.lan>
2017-03-15 23:39:19 +0800

    'little work'

--- unreal.py
+++ unreal.py
@@ -3,8 +3,18 @@ from __future__ import division
 from __future__ import print_function
 
 import tensorflow as tf
-from model.model import UnrealModel
+from model import UnrealNetwork
 
 class UNREAL(object):
   def __init__(self, env, task, visualise):
-    
+    self.env = env
+    self.task = task
+    worker_device = '/job:worker/task:{}'.format(task)
+    with tf.device(tf.train.replica_device_setter(1, worker_device=worker_device)):
+      self.network = UnrealNetwork(env.observation_space.shape, 
+                                  env.action_space.n)
+      self.global_step = tf.get_variable('global_step', [], tf.int32, 
+                                         initializer=tf.constant(0, dtype=tf.int32),
+                                         trainable=False)
+
+    with tf.device(worker_device):
